47093917f184de2ec8ae9a6de60763e04392da47
Kelaris (╯°□°）╯ <kelarislax33@gmail.com>
2022-06-24 10:56:18 +0300

    v1.2 full language name

--- main.py
+++ main.py
@@ -5,31 +5,31 @@ translator = Translator()
 my_file = open(r"C:\Users\vasil\OneDrive\Рабочий стол\Translation.txt", 'w')
 
 languages_dict = {
-    'ar',
-    'zh-tw',
-    'tl',
-    'fr',
-    'ka',
-    'de',
-    'hi',
-    'id',
-    'it',
-    'ja',
-    'ko',
-    'lv',
-    'lt',
-    'no',
-    'fa',
-    'pl',
-    'pt',
-    'ru',
-    'es',
-    'sv',
-    'th',
-    'tr',
-    'uk',
-    'vi',
-    'zu'
+    'ar': 'arabic',
+    'zh-tw': 'chinese (traditional)',
+    'tl': 'filipino',
+    'fr': 'french',
+    'ka': 'georgian',
+    'de': 'german',
+    'hi': 'hindi',
+    'id': 'indonesian',
+    'it': 'italian',
+    'ja': 'japanese',
+    'ko': 'korean',
+    'lv': 'latvian',
+    'lt': 'lithuanian',
+    'no': 'norwegian',
+    'fa': 'persian',
+    'pl': 'polish',
+    'pt': 'portuguese',
+    'ru': 'russian',
+    'es': 'spanish',
+    'sv': 'swedish',
+    'th': 'thai',
+    'tr': 'turkish',
+    'uk': 'ukrainian',
+    'vi': 'vietnamese',
+    'zu': 'zulu',
 }
 var_translation = input('What you want to translate? \n')
 
@@ -39,11 +39,11 @@ def translation():
     out = translator.translate(var_translation, dest=dest_)
     #output = [word.encode('utf-8') for word in out.text]
     print(out.text)
+    print(out.dest)
 
 
 for dest_ in languages_dict:
     translation()
     print(dest_)
 
-my_file.close()
 
